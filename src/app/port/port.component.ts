394fa6e91cfaf3072231be773712674e2e0e2018
Hossam Bassem <hosam9210basem@gmail.com>
2021-02-10 18:59:33 +0200

    second update

--- src/app/port/port.component.ts
+++ src/app/port/port.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { Profile } from "../profile";
 @Component({
   selector: 'app-port',
   templateUrl: './port.component.html',
@@ -8,7 +9,15 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons';
 export class PortComponent implements OnInit {
   plusIcon=faPlus
   constructor() { }
-
+ profiles:Profile[]=
+[
+  {imgSrc:'../../assets/cabin.png',imgIcon:this.plusIcon},
+  {imgSrc:'../../assets/cake.png',imgIcon:this.plusIcon},
+  {imgSrc:'../../assets/circus.png',imgIcon:this.plusIcon},
+  {imgSrc:'../../assets/game.png',imgIcon:this.plusIcon},
+  {imgSrc:'../../assets/safe.png',imgIcon:this.plusIcon},
+  {imgSrc:'../../assets/submarine.png',imgIcon:this.plusIcon}
+]
   ngOnInit(): void {
   }
 
---
+++ src/app/profile.ts
@@ -0,0 +1,5 @@
+export interface Profile {
+    imgSrc: string,
+    imgIcon:any
+
+}
